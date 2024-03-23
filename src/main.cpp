ae9ea24936b587e36928eda53db4e41b52bca511
Priboi18 <bulrafi@gmail.com>
2024-03-23 11:10:21 +0100

    z powrotem na te repozytoruium

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,6 @@ int main(){
     std::cout << "czesc";
     std::cout << "Marcin";
     std::cout << "Kubik";
-      std::cout << "Kubik";
+    std::cout << "Kubik przeszilsmy na inne repozytorium git remote add testaa https://github.com/Priboi18/proba";
     return 0;
 }
